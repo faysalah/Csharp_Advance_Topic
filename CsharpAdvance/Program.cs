9cbc2da89d3b7e2455a7130790fbf3c869af2620
Faysal Ahmmad <faysal725@diu.edu.bd>
2017-11-16 10:32:29 +0600

    Func(with retrun type) and Action(without retrun type) delegate.Those delegates are general, we can assign any data type combination of retrun type and argument type(up to 16)

--- CsharpAdvance/Program.cs
+++ CsharpAdvance/Program.cs
@@ -6,26 +6,43 @@ using System.Threading.Tasks;
 
 namespace CsharpAdvance
 {
-    public delegate T Transformer<T>(T arg);
+    public delegate TResult Func<in T1,in T2, out TResult>(T1 arg1, T2 arg2);
+    public delegate void Action<in T1, in T2>(T1 arg, T2 arg1);
 
-    //class Util
-    //{
-    //    public static void Transform<T>(T[] values, Transformer<T> t)
-    //    {
-    //        for (int i = 0; i < values.Length; i++)
-    //        {
-    //            values[i] = t.Invoke(values[i],values[i]);
-    //        }
-    //    }
-    //}
+    class Util
+    {
+        public static void Transform<T>(T[] values, Func<T,T,T> transformer)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+              values[i] = transformer.Invoke(values[i], values[i]);
+            }
+        }
+
+        public static void Transform1<T>(T[] values, Action<int, double> transformer)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                transformer(120,55.5);
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Transformer t = Square;
-
+            int[] values = { 1, 2, 3 };
+            Util.Transform(values, Square);
+            Util.Transform1(values, Print);
+            foreach (int item in values)
+            {
+                Console.WriteLine(item);
+            }
             Console.Read();
         }
-        static double Square(double x) { return x * x; }
+        static int Square(int x, int y) { return x * y; }
+
+        static void Print(int x, double y) { Console.WriteLine(x + "-----" + y); }
     }
 }
