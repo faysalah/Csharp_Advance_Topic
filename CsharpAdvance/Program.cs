7da9b12662a18c3df3d57c4131e597c72801ffc8
Faysal Ahmmad <faysal725@diu.edu.bd>
2017-11-16 11:07:32 +0600

    Delegate vs Interface, 1. interface defines only a single method 2.Multicast is needed 3.Suscriber needs to implement the interface multiple times.

--- CsharpAdvance/Program.cs
+++ CsharpAdvance/Program.cs
@@ -6,43 +6,41 @@ using System.Threading.Tasks;
 
 namespace CsharpAdvance
 {
-    public delegate TResult Func<in T1,in T2, out TResult>(T1 arg1, T2 arg2);
-    public delegate void Action<in T1, in T2>(T1 arg, T2 arg1);
+    public interface ITransformer
+    {
+        int Transform(int x);
+    }
 
     class Util
     {
-        public static void Transform<T>(T[] values, Func<T,T,T> transformer)
+        public static void TransformAll(int[] values, ITransformer t)
         {
             for (int i = 0; i < values.Length; i++)
             {
-              values[i] = transformer.Invoke(values[i], values[i]);
-            }
-        }
-
-        public static void Transform1<T>(T[] values, Action<int, double> transformer)
-        {
-            for (int i = 0; i < values.Length; i++)
-            {
-                transformer(120,55.5);
+                values[i] = t.Transform(values[i]);
             }
         }
     }
-
+    class Square: ITransformer
+    {
+        public int Transform (int x)
+        {
+            return x * x;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             int[] values = { 1, 2, 3 };
-            Util.Transform(values, Square);
-            Util.Transform1(values, Print);
+        
+            Util.TransformAll(values,new Square());
+
             foreach (int item in values)
             {
                 Console.WriteLine(item);
             }
             Console.Read();
         }
-        static int Square(int x, int y) { return x * y; }
-
-        static void Print(int x, double y) { Console.WriteLine(x + "-----" + y); }
     }
 }
