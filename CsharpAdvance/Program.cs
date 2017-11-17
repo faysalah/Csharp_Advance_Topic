869243fa5501359dea19df88c1a419ea4cc3c0f5
Faysal Ahmmad <faysal725@diu.edu.bd>
2017-11-17 23:22:23 +0600

    Lambda expression similar to arrow function and Anonymous method is method witout name can be assigned to a delegate and this method defination is delegate keyword then prenthesis within mehtod argument finally method body with curly braces

--- CsharpAdvance/Program.cs
+++ CsharpAdvance/Program.cs
@@ -8,16 +8,36 @@ namespace CsharpAdvance
 {
     class Program
     {
+        public delegate int Transformer(int i, int j);
+        
         static void Main(string[] args)
         {
-            var video = new Video() { Title = " VEDIO_1" };
-            var videoEncoder = new VideoEncoder();//broadcaster or publisher
-            var mailService = new MailService();// Subscriber
-            var textMessageService = new TextMessageService();// Subscriber
-            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
-            videoEncoder.VideoEncoded += textMessageService.OnVideoEncoded;
-            videoEncoder.Encode(video);
+            // Anonymous method 2.0
+            Transformer multipication =delegate (int x, int y) { return x * y; };
+            Console.WriteLine(multipication.Invoke(3, 6));
+
+            // Lambda expression 3.0
+            Func<int, int> sqr = x => x * x;
+            Console.WriteLine(sqr(3));
+
+            Func<int, int, int> division = (x,  y) => { return x / y; };
+            Console.WriteLine(division(10,2));
+
+            //Lambda expression capturing outer variable
+            Func<int> natural = Natural();
+            //---vs---
+            //Func<int> natural =() => { int seed = 0; return seed++;};
+            Console.WriteLine(natural.Invoke());
+            Console.WriteLine(natural.Invoke());
+            Console.WriteLine(natural.Invoke());
             Console.Read();
         }
+
+        static Func<int> Natural()
+        {
+            int seed = 0;
+            return () => seed++;
+        }
     }
+
 }
