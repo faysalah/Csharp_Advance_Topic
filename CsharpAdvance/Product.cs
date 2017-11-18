aacaef92e373f2fe2fe2115cc23e8c3490534833
Faysal Ahmmad <faysal725@diu.edu.bd>
2017-11-18 19:55:10 +0600

    Extension method gives syntactic flexibility.It must be static and non generic and must put this keyword to the first parameter

---
+++ CsharpAdvance/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAdvance
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Desciption { get; set; }
+
+    }
+    public static class ProductExtend
+    {
+        public static string UppercaseFirstChar(this string text)
+        {
+            char[] textarray = text.ToCharArray();
+            textarray[0] = Char.ToUpper(textarray[0]);
+            foreach (char item in textarray)
+            {
+                text += item;
+            }
+            return text;
+        }
+
+        public static Product ChangeToDefaultValue(this Product product, string Description)
+        {
+            product.Id = 000;
+            product.Name = "Default_Name";
+            product.Desciption = Description;
+            return product;
+        }
+    }
+}
--- CsharpAdvance/Program.cs
+++ CsharpAdvance/Program.cs
@@ -8,36 +8,13 @@ namespace CsharpAdvance
 {
     class Program
     {
-        public delegate int Transformer(int i, int j);
-        
         static void Main(string[] args)
         {
-            // Anonymous method 2.0
-            Transformer multipication =delegate (int x, int y) { return x * y; };
-            Console.WriteLine(multipication.Invoke(3, 6));
+            List<Product> ListOfProduct = new List<Product>();
+           // IEnumerable<Product> Products = Enumerable.Where(ListOfProduct, x => x.Name == "faysal");
+            IEnumerable<Product> Products = ListOfProduct.Where(x => x.Name == "faysal");
 
-            // Lambda expression 3.0
-            Func<int, int> sqr = x => x * x;
-            Console.WriteLine(sqr(3));
-
-            Func<int, int, int> division = (x,  y) => { return x / y; };
-            Console.WriteLine(division(10,2));
-
-            //Lambda expression capturing outer variable
-            Func<int> natural = Natural();
-            //---vs---
-            //Func<int> natural =() => { int seed = 0; return seed++;};
-            Console.WriteLine(natural.Invoke());
-            Console.WriteLine(natural.Invoke());
-            Console.WriteLine(natural.Invoke());
-            Console.Read();
-        }
-
-        static Func<int> Natural()
-        {
-            int seed = 0;
-            return () => seed++;
+            Console.ReadKey();
         }
     }
-
 }
