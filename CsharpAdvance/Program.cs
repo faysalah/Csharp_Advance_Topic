8cb7e261d2f37b05696e890b8bec44b53d753dd9
Faysal Ahmmad <faysal725@diu.edu.bd>
2018-03-05 02:12:50 +0600

    new keyword used before method name to ignore parent class implement

--- CsharpAdvance/Program.cs
+++ CsharpAdvance/Program.cs
@@ -6,13 +6,37 @@ using System.Threading.Tasks;
 
 namespace CsharpAdvance
 {
+    class A
+    {
+        public void Display() { Console.WriteLine("A::Display()"); }
+    }
+
+    class B : A
+    {
+        public new void Display() { Console.WriteLine("B::Display()"); }
+    }
+
+    class C : B
+    {
+        public new void Display() { Console.WriteLine("C::Display()"); }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            List<Product> ListOfProduct = new List<Product>();
-           // IEnumerable<Product> Products = Enumerable.Where(ListOfProduct, x => x.Name == "faysal");
-            IEnumerable<Product> Products = ListOfProduct.Where(x => x.Name == "faysal");
+            A a = new A();
+            B b = new B();
+            C c = new C();
+
+            a.Display();
+            b.Display();
+            c.Display();
+
+            a = new B();
+            a.Display(); 
+
+            b = new C();
+            b.Display(); 
 
             Console.ReadKey();
         }
