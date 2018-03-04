ff07d590dee3f296a619c85082d788dcaa9a9bc9
Faysal Ahmmad <faysal725@diu.edu.bd>
2018-03-05 02:04:11 +0600

    added new branch for object oriented concept

---
+++ CsharpAdvance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAdvance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> ListOfProduct = new List<Product>();
+           // IEnumerable<Product> Products = Enumerable.Where(ListOfProduct, x => x.Name == "faysal");
+            IEnumerable<Product> Products = ListOfProduct.Where(x => x.Name == "faysal");
+
+            Console.ReadKey();
+        }
+    }
+}
